d7a2c87bbf2b4a736dc57bd5f5319fe3d4edcf8d
Seth Todd <seth.todd@outlook.com>
2019-12-06 01:41:10 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,7 +34,7 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index) == false
     puts "Please enter 1-9:"
-    move(board, user_input)
+    move(board, index)
   end
 end
 
